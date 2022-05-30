e349a6f3017817db12b1f6d3c7b2c8cb4996a02f
nurulfauzh <nrulfauzh@gmail.com>
2022-05-30 13:16:06 +0700

    nurul 3

---
+++ large_produk.py
@@ -0,0 +1,11 @@
+from produk_delivery import ProdukDelivery
+
+class LargeProduk(ProdukDelivery):
+
+    def __init__(self, price : int, name : str) :
+        super().__init__(price, name)
+
+    def calculate_delivery(self) -> int:
+        return self.get_price() + 3000
+
+        
