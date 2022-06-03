05577e80d18b93b75df6c7e2023923de2f49684f
nurulfauzh <nrulfauzh@gmail.com>
2022-06-03 10:17:31 +0700

    nurul

---
+++ soal/penembak.py
@@ -0,0 +1,8 @@
+from karakter import Karakter
+
+class Penembak(Karakter):
+    def __init__(self, nama: str, power: int):
+        super().__init__(nama, power)
+    
+    def menyerang(self)->str:
+        print("Mengeluarkan ", self.get_nama(),self.get_power(),"power menembak")
